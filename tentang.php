f0ac898bd72a2683ba4a4ee1840f68b374117230
febra <m.febras@yahoo.com>
2017-02-23 09:10:21 +0700

    halaman tentang

---
+++ tentang.php
@@ -0,0 +1,26 @@
+<?php
+  include "header.php";
+?>
+
+<div class="page">
+	<div class="container">
+		<h3 class="page-title">Tentang Aplikasi</h3>
+		<p>
+			Aplikasi ini dipergunakan sebagai fasilitas pelayanan publik yang bisa diacces oleh masyarakat luas didalam bidang pelatihan kerja. Dengan adanya aplikasi ini diharapkan masyarakat Kudus dapat lebih mudah melakukan pendaftaran pelatihan di BLK Kudus.
+		</p>
+		<p>
+			Sistem Informasi Pendaftaran Pelatihan ini telah setahun di jalankan dan pada akhir tahun 2016 ini kami update. Ada beberapa penambahan menu dihalaman front-end atau halaman interface dengan pengguna umum. Diantaranya adalah Menu Pendaftaran dengan menggunakan NIK, menu Jadwal dan Pengumuman Hasil seleksi.
+		</p>
+		<p>
+			Penggunaan menu-menu tersebut diharapkan akan mempermudah masyarakat dalam mengikuti pelatihan di BLK Kudus. Selain menu diatas, Pendaftar juga dapat melihat status pendaftaranya melalui login dengan memasukkan email dan password yang diinputkan saat mendaftar.
+		</p>
+	</div>
+</div>
+
+<?php
+  include "footer.php";
+?>
+    
+
+  </body>
+</html>
