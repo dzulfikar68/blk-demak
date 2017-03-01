38132c20feead73e36a329fe1f0fb0a2f8e2cb5f
febra <m.febras@yahoo.com>
2017-03-01 21:23:52 +0700

    pencarian no_registrasi hasil seleksi

--- hasil_seleksi.php
+++ hasil_seleksi.php
@@ -3,19 +3,21 @@
 
 	/* Koneksi ke DB */
     require_once ('proses/koneksi_db.php');
+	require_once ('proses/helper.php');
 
 	$_SESSION['page'] = "hasil_seleksi";
 
+	// ambil daftar peserta lulus
 	$tahun = date('Y');
 	$get_sql = "SELECT registrasi_pelatihan.no_registrasi, jadwal.angkatan, kejuruan.nama_kejuruan, peserta.nama
 				FROM registrasi_pelatihan, jadwal, kejuruan, peserta WHERE registrasi_pelatihan.status='6' AND registrasi_pelatihan.id_jadwal=jadwal.id_jadwal AND registrasi_pelatihan.id_kejuruan=kejuruan.id_kejuruan AND registrasi_pelatihan.id_peserta=peserta.id AND YEAR(jadwal.pelatihan_awal)='$tahun'
 				ORDER BY registrasi_pelatihan.no_registrasi";
 	$result = mysqli_query($connect, $get_sql);
 
+	// pencarian o_registrasi
 	$keyword = $_POST['search'];
-
 	if (!empty($keyword)) {
-		$search = "SELECT registrasi_pelatihan.no_registrasi, jadwal.angkatan, kejuruan.nama_kejuruan, peserta.nama
+		$search = "SELECT registrasi_pelatihan.no_registrasi, jadwal.angkatan, kejuruan.nama_kejuruan, peserta.nama, registrasi_pelatihan.status
 					FROM registrasi_pelatihan, jadwal, kejuruan, peserta WHERE registrasi_pelatihan.no_registrasi='$keyword' AND registrasi_pelatihan.id_jadwal=jadwal.id_jadwal AND registrasi_pelatihan.id_kejuruan=kejuruan.id_kejuruan AND registrasi_pelatihan.id_peserta=peserta.id AND YEAR(jadwal.pelatihan_awal)='$tahun'
 					ORDER BY registrasi_pelatihan.no_registrasi";
 		$result = mysqli_query($connect, $search);
@@ -36,7 +38,7 @@
 					<form action="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/hasil_seleksi.php" method="POST">
 						<div class="form-group col-sm-3">
 							<label>Search</label>
-							<input type="search" name="search" placeholder="Nomor peserta" <?php echo (!empty($keyword)) ? "$keyword" : "";?> >
+							<input type="search" name="search" placeholder="Nomor peserta" value="<?php echo (!empty($keyword)) ? $keyword : '';?>" >
 						</div>
 						<div class="form-group col-sm-3" style="padding-left: 0px;">
 							<input class="btn btn-primary" type="submit" value="Search">
@@ -46,6 +48,42 @@
 			</div>
 			<br>
 			<div>
+				<?php
+					if (!empty($keyword)) {
+				?>
+				<div class="col-sm-10 col-sm-offset-1">
+				<h4 style="font-weight: 600">Hasil Pencarian</h4>
+					<table class="table table-striped green-table">
+						<thead>
+							<tr>
+								<th class="number">No</th>
+								<th>Nomor Pendaftaran</th>
+								<th class="name">Nama</th>
+								<th>Kejuruan</th>
+								<th>Angkatan</th>
+								<th>Status</th>
+							</tr>
+						</thead>
+						<tbody>
+							<?php
+								$no = 1;
+								while ($row=mysqli_fetch_array($result)) {
+									echo "<tr>";
+									echo '<td class="number">'. $no .'</td>';
+									echo '<td>'. $row['no_registrasi'] .'</td>';
+									echo '<td>'. $row['nama'] .'</td>';
+									echo '<td>'. $row['nama_kejuruan'] .'</td>';
+									echo '<td>'. $row['angkatan'] .'</td>';
+									echo '<td>'. convertStatusRegistrasi($row['status']) .'</td>';
+									echo "</tr>";
+								}
+							?>
+						</tbody>
+					</table>
+				<?php
+					}
+					else {
+				?>
 				<h4 class="schedule-title">Daftar Peserta Lulus Registrasi Pelatihan Tahun </h4>
 				<div class="col-sm-8 col-sm-offset-2">
 					<table class="table table-striped green-table">
@@ -73,7 +111,9 @@
 							?>
 						</tbody>
 					</table>
-
+				<?php
+					}
+				?>
 					<!-- Pagination -->
 					<nav aria-label="Page navigation">
 						<ul class="pagination">
